5e3b449f4d2975017e151763114bf99d36bb53c2
Andy Rios <andyrios611@gmail.com>
2024-05-06 18:20:26 -0500

    Herencia y Polimorfismo - Implementación de Clase Empleado por [Andy Rios]

---
+++ src/Empleado.java
@@ -0,0 +1,26 @@
+public class Empleado {
+    private String nombre;
+    private Integer salario;
+
+    public Empleado(String nombre, Integer salario) {
+        this.nombre = nombre;
+        this.salario = salario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getSalario() {
+        return salario;
+    }
+
+    public void setSalario(Integer salario) {
+        this.salario = salario;
+    }
+}
+
