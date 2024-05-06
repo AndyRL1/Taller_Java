084e2a71331276516014126490e55661b5a28bfa
Andy Rios <andyrios611@gmail.com>
2024-05-06 17:38:59 -0500

    Herencia - Implementación de Constructores por [Andy Rios]

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -2,6 +2,13 @@ public class Estudiante extends Persona{
 
     private String grado;
 
+    public Estudiante(String nombre, Integer edad, String fechaNacimiento, String grado) {
+
+        super(nombre, edad, fechaNacimiento);
+
+        this.grado = grado;
+    }
+
     @Override
     public void mensaje(){
         super.mensaje();
