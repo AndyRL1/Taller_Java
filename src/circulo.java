797479e5db7980e01d93677a233fd78ba240cc6a
Andy Rios <andyrios611@gmail.com>
2024-05-06 18:16:32 -0500

    Encapsulamiento y Herencia - Implementación de Métodos para Cálculo de Área y Perímetro por [Andy Rios]

--- src/circulo.java
+++ src/circulo.java
@@ -1,16 +1,20 @@
 public class circulo extends figuraGeometrica{
-    private String radio;
+    private Integer radio;
 
-    public circulo(String nombre, String color, String radio) {
+    public circulo(String nombre, String color, Integer radio) {
         super(nombre, color);
         this.radio = radio;
     }
 
-    public String getRadio() {
+    public Integer getRadio() {
         return radio;
     }
 
-    public void setRadio(String radio) {
+    public void setRadio(Integer radio) {
         this.radio = radio;
+    }
+
+    public void calcularPerimetro(){
+        System.out.println("El perimetro de este rectangulo es de: \n" + (2*3.14*this.radio) + "cm");
     }
 }
--- src/rectangulo.java
+++ src/rectangulo.java
@@ -1,26 +1,34 @@
 public class rectangulo extends figuraGeometrica {
-    private String longitud;
-    private String ancho;
+    private Integer longitud;
+    private Integer ancho;
 
-    public rectangulo(String nombre, String color, String longitud, String ancho) {
+    public rectangulo(String nombre, String color, Integer longitud, Integer ancho) {
         super(nombre, color);
         this.longitud = longitud;
         this.ancho = ancho;
     }
 
-    public String getLongitud() {
+    public Integer getLongitud() {
         return longitud;
     }
 
-    public void setLongitud(String longitud) {
+    public void setLongitud(Integer longitud) {
         this.longitud = longitud;
     }
 
-    public String getAncho() {
+    public Integer getAncho() {
         return ancho;
     }
 
-    public void setAncho(String ancho) {
+    public void setAncho(Integer ancho) {
         this.ancho = ancho;
+    }
+
+    public void calcularArea(){
+        System.out.println("El area de este rectangulo es de: \n" + (this.longitud*this.ancho) + "cm^2");
+    }
+
+    public void calcularPerimetro(){
+        System.out.println("El perimetro de este rectangulo es de: \n" + (this.longitud*2+this.ancho*2) + "cm");
     }
 }
